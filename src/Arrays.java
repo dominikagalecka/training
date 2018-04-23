55c7f793a4a256770b37630ca38d2a4e75d230bb
dominikagalecka <dalecka@gmail.com>
2018-04-23 21:41:01 +0200

    Gitignore

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,10 +2,15 @@ public class Arrays {
     public static void main(String[] args){
         int[] prices={12,13};
 
-        System.out.println(calculateAverage(prices[0],prices[1]));
-        System.out.println(prices[0]);
-        System.out.println(prices[1]);
-        System.out.println(prices[2]);
+        try {
+            System.out.println(calculateAverage(prices[0], prices[1]));
+            System.out.println(prices[0]);
+            System.out.println(prices[1]);
+            System.out.println(prices[2]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("You are trying to display a value that does not exist");
+        }
     }
 
     public static float calculateAverage(int firstProductPrice, int secondProductPrice){
